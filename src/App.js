b7c3a4d3211690ac7b1d92b1778dd41926894f39
Sheeza-Imtiaz <sheezaimtiiaz@gmail.com>
2024-05-10 13:49:20 +0500

    res

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Routes, Route} from 'react-router-dom';
-import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Properties from './components/Properties';
